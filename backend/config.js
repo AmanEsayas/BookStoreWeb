6122d6e5d225f28f556d3fb760ba083d009bf1fc
amanuel <amanuelesayas37@gmail.com>
2024-08-28 12:09:28 +0200

    pushing everything

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+ 'mongodb+srv://root:Strong%21%40psd1234@cluster0.rtmhjca.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0';
